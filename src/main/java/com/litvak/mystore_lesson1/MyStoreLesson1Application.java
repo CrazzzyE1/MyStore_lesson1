0dcae3d84921ab19f48c0b274db7e15864f892f7
ivan <litvakivan@gmail.com>
2021-05-23 14:36:00 +0300

    init com

---
+++ src/main/java/com/litvak/mystore_lesson1/MyStoreLesson1Application.java
@@ -0,0 +1,13 @@
+package com.litvak.mystore_lesson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStoreLesson1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStoreLesson1Application.class, args);
+    }
+
+}
