6f32d66dc5fadf64a25d62205180eaf3779f7a06
litvakivan <litvakivan@gmail.com>
2021-07-18 13:26:25 +0300

    super shop

--- src/main/java/com/litvak/mystore_lesson1/aspect/Logger.java
+++ src/main/java/com/litvak/mystore_lesson1/aspect/Logger.java
@@ -5,8 +5,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,6 +16,9 @@ import java.util.List;
 public class Logger {
 
     public static List<String> loggerInfo = Collections.synchronizedList(new ArrayList<>());
+    LocalDateTime localNow = LocalDateTime.now();
+    ZonedDateTime zonedUTC = localNow.atZone(ZoneId.of("UTC"));
+    ZonedDateTime zonedIST = zonedUTC.withZoneSameInstant(ZoneId.of("Europe/Moscow"));
 
     @Pointcut("execution(* com.litvak.mystore_lesson1.controllers..*.*(..))")
     private void logger() {
@@ -25,37 +27,37 @@ public class Logger {
     @Before("logger()")
     public void logBefore(JoinPoint joinPoint) {
         loggerInfo.add("[" + LocalDate.now() + " " +
-                LocalTime.now().truncatedTo(ChronoUnit.SECONDS) + "] Before: " + joinPoint.getSignature());
+                zonedIST.truncatedTo(ChronoUnit.SECONDS) + "] Before: " + joinPoint.getSignature());
     }
 
     @AfterReturning("logger()")
     public void logAfterReturning(JoinPoint joinPoint) {
         loggerInfo.add("[" + LocalDate.now() + " " +
-                LocalTime.now().truncatedTo(ChronoUnit.SECONDS) + "] AfterReturning: " + joinPoint.getSignature());
+                zonedIST.truncatedTo(ChronoUnit.SECONDS) + "] AfterReturning: " + joinPoint.getSignature());
     }
 
     @AfterThrowing("logger()")
     public void logAfterThrowing(JoinPoint joinPoint) {
         loggerInfo.add("[" + LocalDate.now() + " " +
-                LocalTime.now().truncatedTo(ChronoUnit.SECONDS) + "] AfterThrowing: " + joinPoint.getSignature());
+                zonedIST.truncatedTo(ChronoUnit.SECONDS) + "] AfterThrowing: " + joinPoint.getSignature());
     }
 
     @Around("logger()")
     public Object logAround(ProceedingJoinPoint pjp) {
         Object value;
         loggerInfo.add("[" + LocalDate.now() + " " +
-                LocalTime.now().truncatedTo(ChronoUnit.SECONDS) + "] Around before: " + pjp.getSignature());
+                zonedIST.truncatedTo(ChronoUnit.SECONDS) + "] Around before: " + pjp.getSignature());
         try {
             value = pjp.proceed();
             loggerInfo.add("[" + LocalDate.now() + " " +
-                    LocalTime.now().truncatedTo(ChronoUnit.SECONDS) + "] Around: " + pjp.getSignature());
+                    zonedIST.truncatedTo(ChronoUnit.SECONDS) + "] Around: " + pjp.getSignature());
         } catch (Throwable t) {
             loggerInfo.add("[" + LocalDate.now() + " " +
-                    LocalTime.now().truncatedTo(ChronoUnit.SECONDS) + "] Around with Error: " + pjp.getSignature());
+                    zonedIST.truncatedTo(ChronoUnit.SECONDS) + "] Around with Error: " + pjp.getSignature());
             return null;
         }
         loggerInfo.add("[" + LocalDate.now() + " " +
-                LocalTime.now().truncatedTo(ChronoUnit.SECONDS) + "] Around after: " + pjp.getSignature());
+                zonedIST.truncatedTo(ChronoUnit.SECONDS) + "] Around after: " + pjp.getSignature());
         return value;
     }
 }
