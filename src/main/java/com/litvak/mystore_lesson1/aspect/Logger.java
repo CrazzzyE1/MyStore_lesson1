bbe718b85ba8a3e2d77b8c6ea1ad4b2835e1e93f
litvakivan <litvakivan@gmail.com>
2021-07-18 13:31:27 +0300

    super shop

--- src/main/java/com/litvak/mystore_lesson1/aspect/Logger.java
+++ src/main/java/com/litvak/mystore_lesson1/aspect/Logger.java
@@ -16,9 +16,6 @@ import java.util.List;
 public class Logger {
 
     public static List<String> loggerInfo = Collections.synchronizedList(new ArrayList<>());
-    LocalDateTime localNow = LocalDateTime.now();
-    ZonedDateTime zonedUTC = localNow.atZone(ZoneId.of("UTC"));
-    ZonedDateTime zonedIST = zonedUTC.withZoneSameInstant(ZoneId.of("Europe/Moscow"));
 
     @Pointcut("execution(* com.litvak.mystore_lesson1.controllers..*.*(..))")
     private void logger() {
@@ -26,24 +23,36 @@ public class Logger {
 
     @Before("logger()")
     public void logBefore(JoinPoint joinPoint) {
+        LocalDateTime localNow = LocalDateTime.now();
+        ZonedDateTime zonedUTC = localNow.atZone(ZoneId.of("UTC"));
+        ZonedDateTime zonedIST = zonedUTC.withZoneSameInstant(ZoneId.of("Europe/Moscow"));
         loggerInfo.add("[" + LocalDate.now() + " " +
                 zonedIST.truncatedTo(ChronoUnit.SECONDS) + "] Before: " + joinPoint.getSignature());
     }
 
     @AfterReturning("logger()")
     public void logAfterReturning(JoinPoint joinPoint) {
+        LocalDateTime localNow = LocalDateTime.now();
+        ZonedDateTime zonedUTC = localNow.atZone(ZoneId.of("UTC"));
+        ZonedDateTime zonedIST = zonedUTC.withZoneSameInstant(ZoneId.of("Europe/Moscow"));
         loggerInfo.add("[" + LocalDate.now() + " " +
                 zonedIST.truncatedTo(ChronoUnit.SECONDS) + "] AfterReturning: " + joinPoint.getSignature());
     }
 
     @AfterThrowing("logger()")
     public void logAfterThrowing(JoinPoint joinPoint) {
+        LocalDateTime localNow = LocalDateTime.now();
+        ZonedDateTime zonedUTC = localNow.atZone(ZoneId.of("UTC"));
+        ZonedDateTime zonedIST = zonedUTC.withZoneSameInstant(ZoneId.of("Europe/Moscow"));
         loggerInfo.add("[" + LocalDate.now() + " " +
                 zonedIST.truncatedTo(ChronoUnit.SECONDS) + "] AfterThrowing: " + joinPoint.getSignature());
     }
 
     @Around("logger()")
     public Object logAround(ProceedingJoinPoint pjp) {
+        LocalDateTime localNow = LocalDateTime.now();
+        ZonedDateTime zonedUTC = localNow.atZone(ZoneId.of("UTC"));
+        ZonedDateTime zonedIST = zonedUTC.withZoneSameInstant(ZoneId.of("Europe/Moscow"));
         Object value;
         loggerInfo.add("[" + LocalDate.now() + " " +
                 zonedIST.truncatedTo(ChronoUnit.SECONDS) + "] Around before: " + pjp.getSignature());
